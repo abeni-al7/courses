804a9ca206d8f768249ed21308455ba520460cbc
abeni-al7 <abenezeralebachew3@gmail.com>
2025-03-23 14:29:42 +0300

    Create a data source

--- app/src/main/java/com/example/courses/models/Course.kt
+++
@@ -1,7 +0,0 @@
-package com.example.courses.models
-
-data class Course(
-    val imageResourceId: Int,
-    val title: String,
-    val numberOfCourses: Int
-)
---
+++ app/src/main/java/com/example/courses/models/Topic.kt
@@ -0,0 +1,7 @@
+package com.example.courses.models
+
+data class Topic(
+    val stringResourceId: Int,
+    val numberOfCourses: Int,
+    val imageResourceId: Int
+)
